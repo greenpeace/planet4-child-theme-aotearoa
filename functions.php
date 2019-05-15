90b82fb9e18ea0274294f3fb861b6cfadc0ec68b
Nikos Roussos <nikos@roussos.cc>
2019-05-15 13:17:11 +0300

    Revert to simple css file

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
 
 function enqueue_child_styles() {
-	$css_creation = filectime(get_stylesheet_directory() . '/style.min.css');
+	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.min.css', [], $css_creation );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
