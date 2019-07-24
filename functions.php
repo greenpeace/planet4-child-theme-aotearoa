c44cf6f85abeb4fbd04a3c8ccb66064c3bad9ead
Kitt McGregor <kitt.mcgregor@greenpeace.org>
2019-07-25 11:56:08 +1200

    Update functions.php
    Added debugging

--- functions.php
+++ functions.php
@@ -18,3 +18,19 @@ function enqueue_child_styles() {
 	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
+
+// test 
+add_action('wp_footer', 'my_action_javascript');
+function my_action_javascript() {
+?>
+<script type="text/javascript">
+(function($) {
+
+console.log("Child theme functions jQuery check - release v0.5.2");
+
+})(jQuery);
+
+</script>
+
+<?php
+}
