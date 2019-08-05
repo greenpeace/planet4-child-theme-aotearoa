48d591bedfc10263521f73977709504ced5deacb
Kitt McGregor <kitt.mcgregor@greenpeace.org>
2019-08-05 15:12:16 +1200

    Update functions.php
    Testing is_page condition

--- functions.php
+++ functions.php
@@ -4,14 +4,6 @@
  * Additional code for the child theme goes in here.
  */
 
-setup_postdata($post);
-
-echo '<pre>';
-echo '================================================================';
-echo $post->post_title;
-echo '================================================================';
-echo '</pre>';
-
 
 add_action( 'wp_head', 'gpnz_enqueue_script', 11 );
 function gpnz_enqueue_script() {
@@ -19,7 +11,15 @@ function gpnz_enqueue_script() {
       	wp_enqueue_script( 'phone-format-js', 'phoneFormat.js', false );
 	  	wp_enqueue_script( 'jquery-inputmask-js', 'jquery.inputmask.bundle.min.js', false );
     } else {
-	    add_action('wp_footer', 'console_log_errors');
+	    // condition not met
+    }
+}
+
+// test is_page condition
+add_action( 'wp_head', 'gpnz_is_page', 12 );
+function gpnz_is_page() {
+	if( is_page( 'Validation Test' ) ){
+      add_action('wp_footer', 'console_log_errors');
     }
 }
 
@@ -29,14 +29,12 @@ function enqueue_child_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
 
-// test 
-
 function console_log_errors() {
 ?>
 <script type="text/javascript">
 (function($) {
 
-console.log("condition not found : has_shortcode( $post->post_content, 'shortcake_enblock' ");
+console.log("is_page( 'Validation Test' )");
 
 })(jQuery);
 
