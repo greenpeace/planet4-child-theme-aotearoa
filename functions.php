ebb7d2d909fb7e08092795e715a997823923f4f0
Kitt McGregor <kitt.mcgregor@greenpeace.org>
2019-11-12 10:24:01 +1300

    Update condition if enform
    foreach $block['blockName']

--- functions.php
+++ functions.php
@@ -7,19 +7,23 @@
 add_action( 'wp_head', 'gpnz_enqueue_script', 11 );
 function gpnz_enqueue_script() {
 	global $post;
-	
+		
 	$blocks = parse_blocks( $post->post_content );
- 	if(is_page('test') ){
- 		print_r($blocks);
- 	}
 	
-	if ( has_block('enform') ){
-	    wp_enqueue_script( 'phone-format-js', get_stylesheet_directory_uri().'/phoneFormat.js', false );
-		wp_enqueue_script( 'jquery-inputmask-js', get_stylesheet_directory_uri().'/jquery.inputmask.bundle.min.js', false );
-	} else {
-		add_action('wp_footer', 'console_log');
+	if ( has_blocks( $post->post_content ) ) {
+	    $blocks = parse_blocks( $post->post_content );
+		
+		foreach ( $blocks as $block ) {
+			if ( 'planet4-blocks/enform' === $block['blockName'] ) {
+				add_action('wp_footer', 'console_log_true');
+			    wp_enqueue_script( 'phone-format-js', get_stylesheet_directory_uri().'/phoneFormat.js', false );
+				wp_enqueue_script( 'jquery-inputmask-js', get_stylesheet_directory_uri().'/jquery.inputmask.bundle.min.js', false );
+			} else {
+				add_action('wp_footer', 'console_log_false');
+			}
+		}
+	
 	}
-}
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
 function enqueue_child_styles() {
@@ -27,12 +31,26 @@ function enqueue_child_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
 
-function console_log() {
+function console_log_true() {
 ?>
 <script type="text/javascript">
 (function($) {
 
-console.log(" FAILED: has_block('enform') ");
+console.log(" match: 'planet4-blocks/enform' === $block['blockName'] ");
+
+})(jQuery);
+
+</script>
+
+<?php
+}
+
+function console_log_false() {
+?>
+<script type="text/javascript">
+(function($) {
+
+console.log(" no match: 'planet4-blocks/enform' === $block['blockName'] ");
 
 })(jQuery);
 
