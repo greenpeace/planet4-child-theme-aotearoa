d45302f0f2cfcbd7877bfbd80be77ed0a55977ca
Kitt McGregor <kitt.mcgregor@greenpeace.org>
2019-11-19 09:24:13 +1300

    Update Branch to master

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
 add_action( 'wp_head', 'gpnz_enqueue_script', 11 );
 function gpnz_enqueue_script() {
 	global $post;
+<<<<<<< Updated upstream
 	
 	$blocks = parse_blocks( $post->post_content );
  	if(is_page('test') ){
@@ -19,6 +20,26 @@ function gpnz_enqueue_script() {
 	} else {
 		add_action('wp_footer', 'console_log');
 	}
+=======
+
+	$blocks = parse_blocks( $post->post_content );
+
+	if ( has_blocks( $post->post_content ) ) {
+	    $blocks = parse_blocks( $post->post_content );
+
+		foreach ( $blocks as $block ) {
+			if ( 'planet4-blocks/enform' === $block['blockName'] ) {
+				add_action('wp_footer', 'console_log_true');
+			    wp_enqueue_script( 'phone-format-js', get_stylesheet_directory_uri().'/phoneFormat.js', false );
+				wp_enqueue_script( 'jquery-inputmask-js', get_stylesheet_directory_uri().'/jquery.inputmask.bundle.min.js', false );
+			} else {
+				add_action('wp_footer', 'console_log_false');
+			}
+		}
+
+	}
+
+>>>>>>> Stashed changes
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
