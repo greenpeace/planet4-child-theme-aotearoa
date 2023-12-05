07751e678ce26c1c99db1b679a6c3fe020592388
nirmalrepo <w.v.nirmalfernando@gmail.com>
2023-12-05 14:18:44 +1300

    Removing the typo

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var button = document.createElement("a");
-    bbutton.setAttribute("class", "btn btn-donate btn-donate-mobile-nav");
+    button.setAttribute("class", "btn btn-donate btn-donate-mobile-nav");
     button.setAttribute("href", "https://donate.act.greenpeace.org.nz/p4-donate?source=nav");
     button.setAttribute("data-ga-category", "Menu Navigation");
     button.setAttribute("data-ga-action", "Donate");
