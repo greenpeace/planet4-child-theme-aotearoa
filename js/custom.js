7dadfe11d29a4a27339b53955b1881c123f1d70e
Altan <admin@altan.me>
2025-10-03 14:32:34 +1300

    Replace donate link & linter fomatting

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var button = document.createElement("a");
-    button.setAttribute("class", "btn btn-donate btn-donate-mobile-nav");
-    button.setAttribute("href", "https://donate.act.greenpeace.org.nz/p4-donate?source=nav");
-    button.setAttribute("data-ga-category", "Menu Navigation");
-    button.setAttribute("data-ga-action", "Donate");
-    button.setAttribute("data-ga-label", "Homepage");
-    button.innerHTML = "Donate";
-   
-    var logo = document.querySelector('.site-logo');
-    logo.insertAdjacentElement('afterend', button);
-   });
+document.addEventListener("DOMContentLoaded", (event) => {
+  var button = document.createElement("a");
+  button.setAttribute("class", "btn btn-donate btn-donate-mobile-nav");
+  button.setAttribute(
+    "href",
+    "https://action.greenpeace.org.nz/appeal/donate?utm_source=web&utm_medium=nav&utm_campaign=greenpeace"
+  );
+  button.setAttribute("data-ga-category", "Menu Navigation");
+  button.setAttribute("data-ga-action", "Donate");
+  button.setAttribute("data-ga-label", "Homepage");
+  button.innerHTML = "Donate";
+
+  var logo = document.querySelector(".site-logo");
+  logo.insertAdjacentElement("afterend", button);
+});
